123c075740ddbe3fafffd98fcd902e1d92b68281
thoughtafter <thoughtafter@gmail.com>
2024-09-27 17:49:52 -0700

    evolve_string_parse_f64 to replace stdtod evolve_string_parse_i64 to replace strtoll the libc versions require null terminated strings and errno

--- evolve_inner_core/src/lib.rs
+++ evolve_inner_core/src/lib.rs
@@ -469,6 +469,8 @@ mod bool {
 
 pub mod string {
     use core::cmp::Ordering;
+    use core::str::FromStr;
+    // use libc_print::libc_println;
 
     #[no_mangle]
     extern "Rust" fn evolve_string_cmp(value1: &str, value2: &str) -> i64 {
@@ -480,13 +482,70 @@ pub mod string {
         value1.cmp(value2) == Ordering::Equal
     }
 
+    #[no_mangle]
+    /// return f64 from string, replace strtod
+    /// could use nan to signal error
+    /// or success bool to match similar calls
+    /// better than strtod needing to check errno
+    extern "Rust" fn evolve_string_parse_f64(value: &str) -> (f64, bool) {
+        // let parsed = value.parse::<f64>();
+        let parsed = f64::from_str(value);
+        match parsed {
+            Ok(f64::INFINITY) | Ok(f64::NEG_INFINITY) | Err(_) => (0.0, true),
+            Ok(value) => (value, false),
+        }
+    }
+
+    fn auto_radix(value: &str) -> (&str, u32) {
+        let trimmed = value.trim_start().trim_start_matches('+');
+        // libc_println!("trimmed: {}", trimmed);
+        let hex = trimmed
+            .strip_prefix("0x")
+            .or_else(|| trimmed.strip_prefix("0X"));
+        // libc_println!("hex: {:?}", hex);
+        if let Some(hex) = hex {
+            // libc_println!("found hex: {}", hex);
+            return (hex, 16);
+        };
+
+        // need + removed
+        (trimmed, 10)
+    }
+
+    #[no_mangle]
+    /// return i64 from string
+    /// replace strtoll, no errno
+    ///
+    extern "Rust" fn evolve_string_parse_i64(value: &str, radix: u32) -> (i64, bool) {
+        let (value, radix) = if radix == 0 {
+            auto_radix(value)
+        } else {
+            (value, radix)
+        };
+        if !(2..=36).contains(&radix) {
+            return (0, true);
+        }
+        let parsed = i64::from_str_radix(value, radix);
+        match parsed {
+            Ok(value) => (value, false),
+            Err(_) => (0, true),
+        }
+    }
+
     // #[no_mangle]
     // pub extern "Rust" fn evolve_string_extract(o: Object) -> &str {
     //   from_p o.ptr
     // }
 
     #[cfg(test)]
-    mod tests {}
+    mod tests {
+
+        #[test]
+        fn test_i64_radix() {
+            assert_eq!(i64::from_str_radix("A", 16), Ok(10));
+            // assert_eq!(i64::from_str_radix("-0xa", 16), Ok(10));
+        }
+    }
 }
 
 mod i64 {
@@ -698,8 +757,6 @@ mod mpq {
 // extern "Rust" fn call_from_rust() {
 //     println!("Just called a Rust function from C!");
 // }
-
-
 
 #[cfg(test)]
 mod tests {
