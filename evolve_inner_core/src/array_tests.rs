7a73e5712c210e0c131fb55008a9b67a247c9e5a
thoughtafter <thoughtafter@gmail.com>
2024-12-01 18:51:25 -0800

    more array test from outer to inner

--- evolve_inner_core/src/array_tests.rs
+++ evolve_inner_core/src/array_tests.rs
@@ -1,6 +1,21 @@
 use super::*;
 
 #[test]
+fn test_new() {
+    let a = evolve_array_literal(10);
+    assert_eq!(10, a.capacity());
+    assert_eq!(0, a.len());
+    assert_eq!(10, evolve_array_capacity(a));
+    assert_eq!(0, evolve_array_size(a));
+
+    let a = evolve_array_literal(0);
+    assert_eq!(8, a.capacity());
+    assert_eq!(0, a.len());
+    assert_eq!(8, evolve_array_capacity(a));
+    assert_eq!(0, evolve_array_size(a));
+}
+
+#[test]
 fn test_get_oob() {
     let a = evolve_array_literal(1);
     let b = evolve_array_get(a, 100);
--- evolve_outer_core/src/array.rs
+++ evolve_outer_core/src/array.rs
@@ -117,18 +117,5 @@ mod tests {
         assert_eq!(Object::from(42), b);
     }
 
-    #[test]
-    fn test_new() {
-        let a = evolve_array_literal(10);
-        assert_eq!(10, a.capacity());
-        assert_eq!(0, a.len());
-        assert_eq!(10, evolve_array_capacity(a));
-        assert_eq!(0, evolve_array_size(a));
 
-        let a = evolve_array_literal(0);
-        assert_eq!(8, a.capacity());
-        assert_eq!(0, a.len());
-        assert_eq!(8, evolve_array_capacity(a));
-        assert_eq!(0, evolve_array_size(a));
-    }
 }
